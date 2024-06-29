38d8ee212fdbb4203c9351afe0a2bb209603a61f
SpaceAndFrogs <73168676+SpaceAndFrogs@users.noreply.github.com>
2024-06-29 23:35:32 +0200

    Player movement added
    Player can now move on whole map with wsad and use scroll to zoom

---
+++ Warehouse Manager/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    float horizontalVerticalSpeed;
+
+    [SerializeField]
+    float scrollSpeed;
+
+    [SerializeField]
+    Transform cameraTransform;
+
+    void Update()
+    {
+        CheckForInputs();
+    }
+
+    void CheckForInputs()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.forward*horizontalVerticalSpeed*Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.back*horizontalVerticalSpeed*Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left*horizontalVerticalSpeed*Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right*horizontalVerticalSpeed*Time.deltaTime;
+        }
+
+        if(Input.mouseScrollDelta.y>0)
+        {
+            Vector3 direction = CalculateNormalizedDirection(cameraTransform.position,transform.position);
+
+            cameraTransform.position += direction * scrollSpeed * Time.deltaTime;
+        }
+        else if(Input.mouseScrollDelta.y<0)
+        {
+            Vector3 direction = CalculateNormalizedDirection(transform.position, cameraTransform.position);
+
+            cameraTransform.position += direction * scrollSpeed * Time.deltaTime;
+        }
+
+    }
+
+    Vector3 CalculateNormalizedDirection(Vector3 startPoint, Vector3 endPoint)
+    {
+        return endPoint - startPoint;
+    }
+}

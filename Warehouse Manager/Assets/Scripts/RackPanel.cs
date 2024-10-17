304fdfa9362028802f55146928654cd8f6161ad0
SpaceAndFrogs <73168676+SpaceAndFrogs@users.noreply.github.com>
2024-10-17 19:28:29 +0200

    Added item choosing to rack panel

--- Warehouse Manager/Assets/Scripts/RackPanel.cs
+++ Warehouse Manager/Assets/Scripts/RackPanel.cs
@@ -3,22 +3,49 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using System;
+using UnityEngine.UI;
 
 public class RackPanel : MonoBehaviour
 {
     Rack currentRack = null;
     [SerializeField]
     Panel panel;
+    [SerializeField]
+    Button buttonPrefab;
+    [SerializeField]
+    Items items;
 
     [Serializable]
     public class Panel
     {
-        public GameObject panelObject;
+        public Transform itemsCanvasTransform;
+        public GameObject mainPanelObject;
+        public GameObject choosePanelObject;
         public TextMeshProUGUI itemOnRack;
         public TextMeshProUGUI currentAmountOfItemsOnRack;
 
         public TMP_InputField desiredAmountOfItemsOnRack;
         public TextMeshProUGUI maxAmountOfItemsOnRack;
+    }
+
+    void SetItemButtonsOnPanel()
+    {
+        panel.choosePanelObject.SetActive(true);
+        for(int i = 0; i < items.items.Count; i ++)
+        {
+            int index = i;
+            Button newButton = Instantiate(buttonPrefab,panel.itemsCanvasTransform);
+            newButton.name = "Button: " + items.items[i].name;
+            newButton.GetComponentInChildren<TextMeshProUGUI>().text = items.items[i].name;
+            newButton.onClick.AddListener(() => SetCurrentItem(index));
+        }
+        panel.choosePanelObject.SetActive(false);
+    }
+
+    public void SetCurrentItem(int index)
+    {
+        currentRack.itemOnRack = items.items[index];
+        UpdateRackPanel();
     }
     void Update()
     {
@@ -49,21 +76,41 @@ public class RackPanel : MonoBehaviour
     void Start()
     {
         panel.desiredAmountOfItemsOnRack.onValueChanged.AddListener(ChangeDesiredAmount);
+        SetItemButtonsOnPanel();
     }
 
     void OpenPanelForRack()
+    {
+        UpdateRackPanel();
+        panel.mainPanelObject.SetActive(true);
+    }
+
+    void UpdateRackPanel()
     {
         panel.currentAmountOfItemsOnRack.text = currentRack.amountOfItems.ToString();
         panel.desiredAmountOfItemsOnRack.text = currentRack.desiredAmountOfItems.ToString();
         panel.maxAmountOfItemsOnRack.text = currentRack.maxAmountOfItems.ToString();
         panel.itemOnRack.text = currentRack.itemOnRack.name;
-        panel.panelObject.SetActive(true);
     }
 
     public void ClosePanel()
     {
         currentRack = null;
-        panel.panelObject.SetActive(false);
+        panel.mainPanelObject.SetActive(false);
+    }
+
+    public void OpenChoosePanel(bool openPanel)
+    {
+        if(openPanel)
+        {
+            panel.mainPanelObject.SetActive(false);
+            panel.choosePanelObject.SetActive(true);
+        }else
+        {
+            panel.mainPanelObject.SetActive(true);
+            panel.choosePanelObject.SetActive(false);
+        }
+        
     }
 
     void ChangeDesiredAmount(string newText)

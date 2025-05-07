9c519aed6ee3029459f9fd72c899c45af730d81d
SpaceAndFrogs <73168676+SpaceAndFrogs@users.noreply.github.com>
2025-05-07 21:48:03 +0200

    made

--- Warehouse Manager/Assets/Scripts/HidingObjects.cs
+++ Warehouse Manager/Assets/Scripts/HidingObjects.cs
@@ -6,6 +6,11 @@ public class HidingObjects : MonoBehaviour
 {
     [SerializeField]
     MeshRenderer[] meshRenderer = new MeshRenderer[0];
+    [SerializeField]
+    bool canMove;
+    [SerializeField]
+    float posTolerance = 0.1f;
+    Vector3 lastCheckPos;
 
     void OnEnable() {
         CameraScript.OnCameraMove += CheckHide;
@@ -13,6 +18,24 @@ public class HidingObjects : MonoBehaviour
 
     void OnDisable() {
         CameraScript.OnCameraMove -= CheckHide;
+    }
+
+    void Start()
+    {
+        lastCheckPos = transform.position;
+        CheckHide();
+    }
+
+    void FixedUpdate()
+    {
+        if (canMove)
+        {
+            if (Vector3.Distance(lastCheckPos, transform.position) > posTolerance)
+            {
+                lastCheckPos = transform.position;
+                CheckHide();
+            }
+        }
     }
     
     void CheckHide()
